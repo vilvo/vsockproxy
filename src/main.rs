5f10673f79554e83f865f3b39d961241a2dec45a
Ville Ilvonen <ville.ilvonen@unikie.com>
2024-01-23 11:39:30 +0200

    add vsock listener and set in non-blocking mode
    * fix: use u32 instead of i32
    
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use vsock::{VsockAddr, VsockListener};
+use libc::VMADDR_CID_ANY;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,9 +12,29 @@ fn main() {
     // TODO:
     // https://docs.rs/rs-streams/latest/rs_streams/fn.setvbuf.html
 
-    let local_port: i32 = args[1].parse::<i32>().unwrap();
-    let remote_cid: i32 = args[2].parse::<i32>().unwrap();
-    let remote_port: i32 = args[3].parse::<i32>().unwrap();
-    println!("vsockproxy started (local port {}, remote cid {}, remote port {})",
-             local_port, remote_cid, remote_port);
+    let local_port: u32 = args[1].parse::<u32>().unwrap();
+    let remote_cid: u32 = args[2].parse::<u32>().unwrap();
+    let remote_port: u32 = args[3].parse::<u32>().unwrap();
+    println!(
+        "vsockproxy started (local port {}, remote cid {}, remote port {})",
+        local_port, remote_cid, remote_port
+    );
+
+    let listener_result = VsockListener::bind(&VsockAddr::new(VMADDR_CID_ANY, local_port));
+    let listener = match listener_result {
+        Ok(listener) => listener,
+        Err(error) => {
+            eprintln!("bind and listen failed: {}", error);
+            std::process::exit(1);
+        }
+    };
+
+    let nonblocking_result = listener.set_nonblocking(true);
+    let _ = match nonblocking_result {
+        Err(error) => {
+            eprintln!("Failed to set non block {}", error);
+            std::process::exit(1);
+        }
+        _ => (),
+    };
 }
