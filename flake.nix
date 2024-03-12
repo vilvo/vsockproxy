efee9cc0a028bdd5194366dcc99f5ff6adafa489
Ville Ilvonen <ville.ilvonen@unikie.com>
2024-03-12 13:27:40 +0200

    packages: iperf-vsock
    * package - iperf-vsock
    * devshell -include iperf-vsock
    * test - trial integration testing with NixOS VMs
    
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        nativeBuildInputs = with pkgs; [pkg-config];
+        iperf-vsock = pkgs.callPackage ./packages/iperf-vsock.nix {};
+        buildInputs = with pkgs; [
+          rust-bin.stable.latest.default
+          openssl
+          iperf-vsock
+        ];
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            inherit buildInputs nativeBuildInputs;
+          };
+        }
+    );
+}
---
+++ packages/iperf-vsock.nix
@@ -0,0 +1,22 @@
+{
+  stdenv,
+  pkgs,
+  lib,
+  ...
+}:
+stdenv.mkDerivation {
+  name = "iperf-vsock";
+  src = pkgs.fetchgit {
+    url = "https://github.com/stefano-garzarella/iperf-vsock.git";
+    rev = "dbef83efc8c0f5ad51ffc8252f018adc1c7e9820";
+    sha256 = "1zl64i3fccbyb7fzbzmmyn23zwk4ycsp7dd8r1nppji0l5w8s38f";
+  };
+
+  meta = with lib; {
+    description = "iperf-vsock";
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+  };
+}
